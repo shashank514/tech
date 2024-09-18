e1b5c12b463ca5f617f06f93c01c8e45ffef2881
shashank514 <shashankkp304@gmail.com>
2024-09-18 16:31:44 +0530

    To show the Pie chart details

--- core/service/expenses/getExpenses.go
+++ core/service/expenses/getExpenses.go
@@ -7,10 +7,11 @@ import (
 	"github.com/tech/core/domain"
 )
 
-func (t *Expenses) GetUserExpenses(ctx context.Context, user *domain.User, month int, year int) domain.Response {
+func (t *Expenses) GetUserExpenses(ctx context.Context, user *domain.User, month string, year int) domain.Response {
 	funcName := "GetUserExpenses"
 	response := domain.UserExpenseResponse{}
 	addCategoryNames := make(map[string]int)
+	addDateAndAmount := make(map[int]int)
 	var CategoryLabels []string
 	var CategoriesExpenses []string
 	var DateLabels []string
@@ -18,31 +19,11 @@ func (t *Expenses) GetUserExpenses(ctx context.Context, user *domain.User, month
 
 	fmt.Println("user id ", user.Id, "month ", month, "year ", year)
 
-	userDetailsByDate, err := t.expensePersistence.ExpenseDatePersistence.GetYpExpenseDateById(user.Id, mapIdAndMonth[month], year)
-	if err != nil {
-		fmt.Println(funcName, err)
-		return domain.Response{Code: "452", Msg: "err.Error()"}
-	}
-	fmt.Println(" userDetailsByDate ", userDetailsByDate)
-
-	if userDetailsByDate == nil {
-		fmt.Println(funcName, "no Expenses of user err :", err)
-		return domain.Response{Code: "453", Msg: "expenses not found"}
-	}
-
-	for _, details := range userDetailsByDate {
-		DateLabels = append(DateLabels, cast.ToString(details.Date))
-		DateExpenses = append(DateExpenses, cast.ToString(details.Amount))
-	}
-	response.DateLabels = DateLabels
-	response.DateExpenses = DateExpenses
-
-	userAllDetails, err := t.expensePersistence.ExpenseDetailsPersistence.GetYpExpenseDateById(user.Id, mapIdAndMonth[month], year)
+	userAllDetails, err := t.expensePersistence.ExpenseDetailsPersistence.GetYpExpenseDateById(user.Id, month, year)
 	if err != nil {
 		fmt.Println(funcName, "no Expenses of user err :", err)
 		return domain.Response{Code: "452", Msg: "err.Error()"}
 	}
-	fmt.Println(" userAllDetails ", userAllDetails)
 
 	if userAllDetails == nil {
 
@@ -60,6 +41,33 @@ func (t *Expenses) GetUserExpenses(ctx context.Context, user *domain.User, month
 	}
 	response.CategoryLabels = CategoryLabels
 	response.CategoriesExpenses = CategoriesExpenses
+
+	userDetailsByDate, err := t.expensePersistence.ExpenseDatePersistence.GetYpExpenseDateById(user.Id, month, year)
+	if err != nil {
+		fmt.Println(funcName, err)
+		return domain.Response{Code: "452", Msg: "err.Error()"}
+	}
+
+	if userDetailsByDate == nil {
+		fmt.Println(funcName, "no Expenses of user err :", err)
+		return domain.Response{Code: "453", Msg: "expenses not found"}
+	}
+
+	for _, details := range userDetailsByDate {
+		addDateAndAmount[details.Date] = cast.ToInt(details.Amount)
+	}
+
+	for i := 1; i <= 31; i++ {
+		DateLabels = append(DateLabels, cast.ToString(i))
+		DateExpenses = append(DateExpenses, cast.ToString(addDateAndAmount[i]))
+	}
+
+	//for _, details := range userDetailsByDate {
+	//	DateLabels = append(DateLabels, cast.ToString(details.Date))
+	//	DateExpenses = append(DateExpenses, cast.ToString(details.Amount))
+	//}
+	response.DateLabels = DateLabels
+	response.DateExpenses = DateExpenses
 
 	return domain.Response{Code: "200", Msg: "success", Model: response}
 }
