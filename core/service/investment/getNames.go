90dd58c9e42d38a3e082312d9e48107e13929ee5
shashank514 <shashankkp304@gmail.com>
2024-09-26 11:28:00 +0530

    Bank Name

--- core/service/investment/getNames.go
+++ core/service/investment/getNames.go
@@ -21,6 +21,42 @@ func AllInvestmentService(message message.MessageDriver, investment investment.I
 	}
 }
 
+var bankName = []string{
+	"HDFC Bank",
+	"State Bank of India",
+	"ICICI Bank",
+	"Punjab National Bank",
+	"Axis Bank",
+	"Kotak Mahindra Bank",
+	"Bank of Baroda",
+	"Canara Bank",
+	"IndusInd Bank",
+	"IDBI Bank",
+	"Bank of India",
+	"Union Bank of India",
+	"Central Bank of India",
+	"Indian Bank",
+	"Indian Overseas Bank",
+	"UCO Bank",
+	"Yes Bank",
+	"IDFC FIRST Bank",
+	"Federal Bank",
+	"RBL Bank",
+	"South Indian Bank",
+	"Bandhan Bank",
+	"Jammu & Kashmir Bank",
+	"Punjab & Sind Bank",
+	"Karur Vysya Bank",
+	"Dhanlaxmi Bank",
+	"City Union Bank",
+	"Tamilnad Mercantile Bank",
+	"Suryoday Small Finance Bank",
+	"AU Small Finance Bank",
+	"Ujjivan Small Finance Bank",
+	"Equitas Small Finance Bank",
+	"Fincare Small Finance Bank",
+}
+
 func (s *Investment) GetSelectNames(ctx context.Context, user *domain.User, category string) domain.Response {
 	funcName := "GetSelectNames"
 	var response []*domain.StockNameInJson
@@ -31,6 +67,10 @@ func (s *Investment) GetSelectNames(ctx context.Context, user *domain.User, cate
 		response, err = s.investment.StockNamePersistence.GetAllStockNames()
 		if err != nil {
 			fmt.Println(funcName, " error while getting stock name err : ", err)
+		}
+	case "FD":
+		for _, name := range bankName {
+			response = append(response, &domain.StockNameInJson{Name: name})
 		}
 	}
 	return domain.Response{Code: "200", Msg: "success", Model: response}
